aace9971ccbb30843497af0a7f8e64c95b683d00
james almeida <james.almeida.ti@gmail.com>
2018-04-17 12:13:07 -0300

    Melhorar logica menu responsivo, iniciando carousel livros

--- js/script.js
+++ js/script.js
@@ -1,27 +1,46 @@
 let oracao = 'A verdadeira medida de um homem não se vê na forma como se comporta em momentos de conforto e conveniência, mas em como se mantém em tempos de controvérsia e desafio.';
 
-function escreverFrase(){
-    for(let i = 0; i < oracao.length; i++){
-        (function(j) {
-            setTimeout(function() {
+function escreverFrase() {
+    for (let i = 0; i < oracao.length; i++) {
+        (function (j) {
+            setTimeout(function () {
                 let span = document.querySelector('.text-main').innerHTML += oracao[j];
-                
+
             }, 120 * i)
         })(i)
     }
 }
 
-window.onload = function(){
-    escreverFrase()
-}
+escreverFrase()
+
 let togle = document.querySelector('.togle-menu');
-
 let mobileMenu = document.querySelector('.menu-mobile ul');
+let itemLista = document.querySelectorAll('li')
 
-togle.onclick = function(){
-    if (mobileMenu.style.display == 'none'){
-        mobileMenu.style = `display: block`;
+togle.onclick = function () {
+    if (mobileMenu.style.transform == 'translateY(-150%)') {
+        mobileMenu.style.transform = `translateY(0)`;
     } else {
-        mobileMenu.style = `display: none`;
+        mobileMenu.style.transform = `translateY(-150%)`;
     }
+
+}
+
+for (item of itemLista) {
+    item.onclick = function () {
+        mobileMenu.style.transform = `translateY(-150%)`;
+    }
+}
+
+// LIVROS SLIDER
+
+let btnNext = document.querySelector('.next');
+let btnPrev = document.querySelector('.prev');
+let carousel = document.querySelectorAll('.carousel');
+
+let carouselAtivo = 0;
+
+btnNext.onclick = function(){
+    carousel[carouselAtivo].style.transform = `translateX(150%)`;
+    carouselAtivo++;
 }
